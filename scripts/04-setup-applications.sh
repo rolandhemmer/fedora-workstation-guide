09d0d184e8c332959e25988c5dafa2fb8f969b2e
Roland Hemmer <rolandhemmer@users.noreply.github.com>
2023-01-09 20:45:21 +0100

    refactor: update VSCodium RPM installation instructions

--- scripts/04-setup-applications.sh
+++ scripts/04-setup-applications.sh
@@ -168,8 +168,8 @@ log_progress "Installing Visual Studio Codium"
 #
 # If you want to use the embedded terminal inside of VS, prefer the RPM installation:
 #
-# > sudo rpm --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
-# > printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee /etc/yum.repos.d/vscodium.repo
+# > sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
+# > printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee --append /etc/yum.repos.d/vscodium.repo
 # > sudo dnf install --assumeyes codium
 
 flatpak_install com.vscodium.codium
