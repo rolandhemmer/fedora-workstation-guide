a6ded41b92d02bc8e3ad867941f3432d9c449353
Roland Hemmer <rolandhemmer@users.noreply.github.com>
2023-01-12 11:12:25 +0100

    fix: update Vim Powerline configuration

--- scripts/05-setup-theme.sh
+++ scripts/05-setup-theme.sh
@@ -81,12 +81,13 @@ fi
 EOT
 
 tee --append ~/.vimrc >$NO_OUTPUT 2>&1 <<EOT
-if [ -f $(which powerline-daemon) ]; then
-    powerline-daemon --quiet
-    POWERLINE_BASH_CONTINUATION=1
-    POWERLINE_BASH_SELECT=1
-    . /usr/share/powerline/bash/powerline.sh
-fi
+python3 from powerline.vim import setup as powerline_setup
+python3 powerline_setup()
+python3 del powerline_setup
+set laststatus=2 " Always display the statusline in all windows
+set showtabline=2 " Always display the tabline, even if there is only one tab
+set noshowmode " Hide the default mode text (e.g. -- INSERT -- below the statusline)
+set t_Co=256
 EOT
 
 sudo cp ~/.bashrc /root/.bashrc >$NO_OUTPUT 2>&1
